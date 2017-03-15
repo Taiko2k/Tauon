91f2afb165a42e6459dd74aa911b3333725e0c42
Taiko2k <Taiko2k@users.noreply.github.com>
2017-03-16 00:18:52 +1300

    code cleanup

---
+++ t_extra.py
@@ -0,0 +1,160 @@
+
+# Tauon Music Box - Misc Functions Module
+
+# Copyright © 2015-2016, Taiko2k captain(dot)gxj(at)gmail.com
+
+#     This file is part of Tauon Music Box.
+#
+#     Tauon Music Box is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Tauon Music Box is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public License
+#     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE
+
+import time
+
+
+class Timer:  # A seconds based timer
+    def __init__(self):
+        self.start = 0
+        self.end = 0
+
+    def set(self):  # Reset
+        self.start = time.time()
+
+    def hit(self):  # Return time and reset
+
+        self.end = time.time()
+        elapsed = self.end - self.start
+        self.start = time.time()
+        return elapsed
+
+    def get(self):  # Return time only
+        self.end = time.time()
+        return self.end - self.start
+
+
+# Removes whatever this is from a line, I forgot
+def rm_16(line):
+    if "ÿ þ" in line:
+        for c in line:
+            line = line[1:]
+            if c == 'þ':
+                break
+
+        line = line[::2]
+    return line
+
+
+# Returns a string from seconds to a compact time format, e.g 2h:23
+def get_display_time(seconds):
+    result = divmod(int(seconds), 60)
+    if result[0] > 99:
+        result = divmod(result[0], 60)
+        return str(result[0]) + 'h ' + str(result[1]).zfill(2)
+    return str(result[0]).zfill(2) + ":" + str(result[1]).zfill(2)
+
+
+# Creates a string from number of bytes to X MB/kB etc
+def get_filesize_string(file_bytes):
+    if file_bytes < 1000:
+        line = str(file_bytes) + " Bytes"
+    elif file_bytes < 1000000:
+        file_kb = round(file_bytes / 1000, 2)
+        line = str(file_kb).rstrip('0').rstrip('.') + " KB"
+    else:
+        file_mb = round(file_bytes / 1000000, 2)
+        line = str(file_mb).rstrip('0').rstrip('.') + " MB"
+    return line
+
+
+# Estimates the perceived luminance of a colour
+def test_lumi(c1):
+    return 1 - (0.299 * c1[0] + 0.587 * c1[1] + 0.114 * c1[2]) / 255
+
+
+# Gives the sum of first 3 elements in a list
+def colour_value(c1):
+    return c1[0] + c1[1] + c1[2]
+
+
+# Performs alpha blending of one colour onto another
+def alpha_blend(colour, base):
+    alpha = colour[3] / 255
+    return [int(alpha * colour[0] + (1 - alpha) * base[0]),
+            int(alpha * colour[1] + (1 - alpha) * base[1]),
+            int(alpha * colour[2] + (1 - alpha) * base[2]), 255]
+
+
+# Change the alpha component of an RGBA list
+def alpha_mod(colour, alpha):
+    return [colour[0], colour[1], colour[2], alpha]
+
+
+# Shift between two colours based on x where x is between 0 and limit
+def colour_slide(a, b, x, x_limit):
+
+    return (min(int(a[0] + ((b[0] - a[0]) * (x / x_limit))), 255),
+     min(int(a[1] + ((b[1] - a[1]) * (x / x_limit))), 255),
+     min(int(a[2] + ((b[2] - a[2]) * (x / x_limit))), 255), 255)
+
+
+# Converts string containing colour in format x,x,x,x(optional) to list
+def get_colour_from_line(cline):
+    colour = ["", "", "", ""]
+
+    mode = 0
+
+    for i in cline:
+
+        if i.isdigit():
+            colour[mode] += i
+        elif i == ',':
+            mode += 1
+
+    for b in range(len(colour)):
+        if colour[b] == "":
+            colour[b] = "255"
+        colour[b] = int(colour[b])
+
+    return colour
+
+# Checks if the numbers in a list are the same
+def checkEqual(lst):
+    return not lst or lst.count(lst[0]) == len(lst)
+
+
+# Gives a score from 0-7 based on number of seconds
+def star_count(sec, dur):
+    stars = 0
+    if sec / dur > 0.95:
+        stars += 1
+    if sec > 60 * 15:
+        stars += 1
+    if sec > 60 * 30:
+        stars += 1
+    if sec > 60 * 60:
+        stars += 1
+    if sec > 60 * 60 * 2:
+        stars += 1
+    if sec > 60 * 60 * 5:
+        stars += 1
+    if sec > 60 * 60 * 10:
+        stars += 1
+    return stars
+
