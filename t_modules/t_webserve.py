e1e0e67e085de8d3dd5e4ced31685235b54ece7b
Taiko2k <Taiko2k@users.noreply.github.com>
2019-01-25 19:37:27 +1300

    removed web remote

---
+++ t_modules/t_webserve.py
@@ -0,0 +1,111 @@
+
+# Tauon Music Box - Web interface module
+
+# Copyright © 2015-2019, Taiko2k captain(dot)gxj(at)gmail.com
+
+#     This file is part of Tauon Music Box.
+#
+#     Tauon Music Box is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Tauon Music Box is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
+
+import html
+
+
+def webserve(pctl, prefs, gui, album_art_gen, install_directory):
+
+    if prefs.enable_web is False:
+        return 0
+    try:
+        from flask import Flask, redirect, send_file, abort, request, jsonify, render_template
+    except:
+        print("Failed to load Flask")
+        gui.show_message("Web server failed to start.", 'warning', "Required dependency 'flask' was not found.")
+        return 0
+
+    gui.web_running = True
+    app = Flask(__name__)
+
+    def shutdown_server():
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+
+    @app.route('/shutdown', methods=['POST'])
+    def shutdown():
+        shutdown_server()
+        gui.web_running = False
+        gui.show_message("Web server stopped.")
+        return 'Server shutting down...'
+
+    @app.route('/radio/')
+    def radio():
+        print("Radio Accessed")
+        return send_file(install_directory + "/templates/radio.html" )
+
+    @app.route('/radio/radio.js')
+    def radio_js():
+        return send_file(install_directory + "/templates/radio.js")
+
+    @app.route('/radio/jquery.js')
+    def radio_jq():
+        return send_file(install_directory + "/templates/jquery.js")
+
+    @app.route('/radio/theme.css')
+    def radio_css():
+        return send_file(install_directory + "/templates/theme.css")
+
+    @app.route('/radio/update_radio', methods=['GET'])
+    def update_radio():
+
+        if pctl.broadcast_active:
+            track = pctl.master_library[pctl.broadcast_index]
+            if track.length > 2:
+                position = pctl.broadcast_time / track.length
+            else:
+                position = 0
+            return jsonify(position=position, index=track.index)
+        else:
+            return jsonify(position=0, index=0)
+
+    @app.route('/radio/getpic', methods=['GET'])
+    def get64pic_radio():
+
+        if pctl.broadcast_active:
+            index = pctl.broadcast_index
+            track = pctl.master_library[index]
+
+            # Lyrics ---
+            lyrics = ""
+            if prefs.radio_page_lyrics and track.lyrics != "":
+                lyrics = html.escape(track.lyrics).replace("\r\n", "\n").replace("\r", "\n").replace("\n", "<br>")
+
+            try:
+                index = track.index
+                base64 = album_art_gen.get_base64(index, (300, 300)).decode()
+
+                return jsonify(index=index, image=base64, title=track.title, artist=track.artist, lyrics=lyrics)
+            except:
+                return jsonify(index=index, image="None", title=track.title, artist=track.artist, lyrics=lyrics)
+        else:
+            return jsonify(index=-1, image="None", title="", artist="- - Broadcast Offline -", lyrics="")
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_file(install_directory + "/gui/favicon.ico", mimetype='image/x-icon')
+
+    if prefs.expose_web is True:
+        app.run(host='0.0.0.0 ', port=prefs.server_port)
+    else:
+        app.run(port=prefs.server_port)
+
