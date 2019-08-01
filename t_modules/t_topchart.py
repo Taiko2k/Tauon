de1b56da684645ae4ad9375ee0818c1b8b32044d
Taiko2k <Taiko2k@users.noreply.github.com>
2019-08-01 23:33:54 +1200

    fix warnings

--- t_modules/t_topchart.py
+++ t_modules/t_topchart.py
@@ -1,4 +1,26 @@
+# Tauon Music Box - Album chart image generator
 
+# Copyright © 2015-2018, Taiko2k captain(dot)gxj(at)gmail.com
+
+#     This file is part of Tauon Music Box.
+#
+#     Tauon Music Box is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Tauon Music Box is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import gi
+gi.require_version('Pango', '1.0')
+gi.require_version('PangoCairo', '1.0')
 import cairo
 from gi.repository import Pango
 from gi.repository import PangoCairo
