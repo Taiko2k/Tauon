f1b2ce6de2e8b8e9cd99e9e6bdc6ce4da45b223e
Taiko2k <Taiko2k@users.noreply.github.com>
2019-09-22 15:36:56 +1200

    small cleanup

--- t_modules/t_gdk_extra.py
+++ t_modules/t_gdk_extra.py
@@ -1,10 +1,30 @@
+# Tauon Music Box - System cursor loader
+
+# Copyright © 2015-2019, Taiko2k captain(dot)gxj(at)gmail.com
+
+#     This file is part of Tauon Music Box.
+#
+#     Tauon Music Box is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Tauon Music Box is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
+
 import gi
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
 from sdl2 import *
 import ctypes
 
-def cursor_gen(type):
+
+def cursor_get_gdk(type):
 
     if type == 4:
         cursor = Gdk.Cursor(Gdk.CursorType.BOTTOM_RIGHT_CORNER)
@@ -22,9 +42,8 @@ def cursor_gen(type):
 
     return Gdk.Cursor.get_surface(cursor)
 
-def cairo_cursor_to_sdl(cairo_surface, x_hot, y_hot):
 
-    #cairo_surface, x_hot, y_hot = Gdk.Cursor.get_surface(cursor)
+def cairo_cursor_to_sdl(cairo_surface, x_hot, y_hot):
 
     w = cairo_surface.get_width()
     h = cairo_surface.get_height()
@@ -32,9 +51,5 @@ def cairo_cursor_to_sdl(cairo_surface, x_hot, y_hot):
     mem = cairo_surface.get_data()
     buff = ctypes.c_buffer(mem.tobytes())
 
-    #return buff, round(x_hot), round(y_hot)
-
-    sdl_surface = SDL_CreateRGBSurfaceWithFormatFrom(ctypes.pointer(buff), w, h, 24, w * 4, SDL_PIXELFORMAT_ARGB8888)
+    sdl_surface = SDL_CreateRGBSurfaceWithFormatFrom(ctypes.pointer(buff), w, h, 32, w * 4, SDL_PIXELFORMAT_ARGB8888)
     return SDL_CreateColorCursor(sdl_surface, round(x_hot), round(y_hot))
-
-
