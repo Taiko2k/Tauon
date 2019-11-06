cf3fa55946429edbdfdaf9c6de315609ea9a5b0e
Taiko2k <Taiko2k@users.noreply.github.com>
2019-11-06 13:14:51 +1300

    remove radio auto play

--- templates/radio.js
+++ templates/radio.js
@@ -34,28 +34,28 @@ function update() {
                     }
 
                     if (status == 1 && data.index == -1) {
+                        // The stream has stopped
                         status = 2;
-                        sound.stop();
                     }
 
                     if (status == 2 && data.index > -1){
+                        // Flush player if stream starts again
                         status = 1;
+
                         sound.currentTime = 0;
                         sound.load();
-                        sound.play();
                     }
 
                     if (status == 0 && data.index > -1) {
+                        // Load player for the first time when stream starts
                         status = 1;
                         console.log(window.location.hostname + ":" + data.port);
-
                         sound.id       = 'audio-player';
                         sound.controls = 'controls';
                         sound.src      = "http://" + window.location.hostname + ":" + data.port;
                         sound.type     = 'audio/ogg';
-
                         document.getElementById('player').appendChild(sound);
-                        sound.play();
+
                     }
                 });
         }
