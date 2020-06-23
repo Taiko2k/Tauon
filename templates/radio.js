c284f56a3c981ee974f7bdcb4a0f6661a678fa19
Taiko2k <Taiko2k@users.noreply.github.com>
2020-06-23 15:32:37 +1200

    update radio page

--- templates/radio.js
+++ templates/radio.js
@@ -8,57 +8,68 @@ sound.controls = 'controls';
 sound.type     = 'audio/ogg';
 sound.preload  = 'none';
 
+var request = new XMLHttpRequest();
+
 function setArt(){
 
-  $.getJSON("/radio/getpic",
-      function(data) {
-          index = data.index
-          if (data.image != 'None'){
-            $("#picture").attr('src', 'data:image/jpeg;base64,' + data.image)
-          }
-          else {
-            $("#picture").attr('src', '#')
-          }
+    request.open('GET', '/radio/getpic', true);
+    request.onload = function() {
 
-          $('#title-text').text(data.artist + " - " + data.title)
-          $('#lyrics').html(data.lyrics)
+        var data = JSON.parse(this.response);
 
-      });
+        index = data.index
+        if (data.image != 'None'){
+            document.getElementById("picture").src = 'data:image/jpeg;base64,' + data.image
+        }
+        else {
+             document.getElementById("picture").src = '#'
+        }
+
+        document.getElementById("title-text").innerText = data.artist + " - " + data.title
+        document.getElementById("lyrics").innerHTML = data.lyrics
+
+      };
+
+    request.send();
 }
 
 function update() {
-            connect_fault = connect_fault - 1
-            $.getJSON("/radio/update_radio",
-                function(data) {
-                    connect_fault = connect_fault + 1
-                    $("#seekfront").width(Math.round(data.position * 700))
+    connect_fault = connect_fault - 1
+    request.open('GET', '/radio/update_radio', true);
+    request.onload = function() {
+        var data = JSON.parse(this.response);
 
-                    if (data.index != index){
-                      setArt();
-                    }
+        connect_fault = connect_fault + 1
+        document.getElementById('seekfront').style.width = Math.round(data.position * 700) + "px";
 
-                    if (status == 1 && data.index == -1) {
-                        // The stream has stopped
-                        status = 2;
-                        sound.src = "";
-                        sound.load();
-                    }
-
-                    if (status == 2 && data.index > -1){
-                        // Stream starts again
-                        status = 1;
-                        sound.src = "http://" + window.location.hostname + ":" + data.port;
-
-                    }
-
-                    if (status == 0 && data.index > -1) {
-                        // Show player for the first time when stream starts
-                        status = 1;
-                        sound.src = "http://" + window.location.hostname + ":" + data.port;
-                        document.getElementById('player').appendChild(sound);
-                    }
-                });
+        if (data.index != index){
+          setArt();
         }
+
+        if (status == 1 && data.index == -1) {
+            // The stream has stopped
+            status = 2;
+            sound.src = "";
+            sound.load();
+        }
+
+        if (status == 2 && data.index > -1){
+            // Stream starts again
+            status = 1;
+            sound.src = "http://" + window.location.hostname + ":" + data.port;
+
+        }
+
+        if (status == 0 && data.index > -1) {
+            // Show player for the first time when stream starts
+            status = 1;
+            sound.src = "http://" + window.location.hostname + ":" + data.port;
+            document.getElementById('player').appendChild(sound);
+        }
+        };
+
+    request.send();
+}
 
 function tick(){
 
@@ -66,6 +77,5 @@ function tick(){
   if (connect_fault > 0) { setTimeout(tick, 5000); }
   else { alert("Connection lost, reload page to continue.")}
 }
-
 
 tick()
