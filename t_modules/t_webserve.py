6ed0beea625e74e85540ddeac4b3a5238afddb22
Taiko2k <Taiko2k@users.noreply.github.com>
2020-12-27 19:08:44 +1300

    fix

--- t_modules/t_webserve.py
+++ t_modules/t_webserve.py
@@ -254,7 +254,7 @@ def webserve2(pctl, prefs, gui, album_art_gen, install_directory, strings, tauon
             data["id"] = track.index
             data["position"] = track_position
             data["album_id"] = album_id
-            data["has_lyrics"] = track.lyrics == True
+            data["has_lyrics"] = track.lyrics != ""
             data["track_number"] = str(track.track_number).lstrip("0")
             data["can_download"] = not track.is_cue and not track.is_network
 
