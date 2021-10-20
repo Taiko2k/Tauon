5652e9db0ba32cdc606778a8fab5cd403df5e334
Taiko2k <taiko2k@users.noreply.github.com>
2021-10-20 23:36:45 +1300

    fix

---
+++ t_modules/t_launch.py
@@ -0,0 +1,10 @@
+
+class Launch:
+    def __init__(self, tauon, pctl, gui, ddt):
+        self.tauon = tauon
+        self.pctl = pctl
+        self.gui = gui
+        self.ddt = ddt
+
+    def render(self):
+        pass
