723de1a781f1912e72aa202e108202c80c913a90
Pouria Rezaei <Pouria.rz@outlook.com>
2022-02-07 15:47:43 +0330

    Improvement + polish.
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>

--- extra/repo-uninstall.sh
+++ extra/repo-uninstall.sh
@@ -3,7 +3,13 @@
 # Removes everything 'extra/repo-install.sh' creates.
 
 RepoDir=$(realpath $(dirname "$0")/..)
+ShareDir=~/.local/share
 cd $RepoDir
 
-rm -f extra/tauonmb.tmp.* ~/.local/share/{applications/tauonmb.desktop,icons/hicolor/scalable/apps/tauonmb{.svg,-symbolic.svg}}
+set -e
+
+rm -f extra/tauonmb.tmp.* $ShareDir/{applications/tauonmb.desktop,locale,icons/hicolor/scalable/apps/tauonmb{.svg,-symbolic.svg}}
 sudo update-desktop-database
+
+echo -e "Uninstall finished.\
+\n"
