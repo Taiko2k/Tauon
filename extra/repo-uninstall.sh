1ebbeff44d96cc748e35d78915adda0465163aca
RSKYS <Pouria.rz@outlook.com>
2022-02-08 03:05:47 +0330

    Fix repo install/uninstall (#674)
    * Improvement + polish.
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Fix
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Final fixes
    
     - "locale" can be read locally, so leaving .mo's there.
     - We only need *.tmp.* for a second!
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Fix + few typo
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>

--- extra/repo-uninstall.sh
+++ extra/repo-uninstall.sh
@@ -3,7 +3,14 @@
 # Removes everything 'extra/repo-install.sh' creates.
 
 RepoDir=$(realpath $(dirname "$0")/..)
+ShareDir=~/.local/share
 cd $RepoDir
 
-rm -f extra/tauonmb.tmp.* ~/.local/share/{applications/tauonmb.desktop,icons/hicolor/scalable/apps/tauonmb{.svg,-symbolic.svg}}
+set -e
+
+rm -f $ShareDir/{applications/tauonmb.desktop,icons/hicolor/scalable/apps/tauonmb{.svg,-symbolic.svg}}
 sudo update-desktop-database
+
+echo -e "\n== == == == == == ==\n\
+ Uninstall finished.\n\
+== == == == == == =="
