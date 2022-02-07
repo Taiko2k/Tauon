532edd3e94c2545103d9564cee78da63926d6143
RSKYS <Pouria.rz@outlook.com>
2022-02-08 03:05:47 +0330

    Fix repo install/uninstall (#674)
    * Improvement + polish.
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Fix
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Final fixes
    
     - "locale" can be read locally, so leaving .mo's there.
     - We only need *.tmp.* for a second!
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>
    
    * Fix + few typo
    
    Signed-off-by: Pouria Rezaei <Pouria.rz@outlook.com>

--- extra/repo-install.sh
+++ extra/repo-install.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 #
 # A script for the lazy folk and probably a butchering of git.
-# Run this script once you have ALL the dependencies set up and updated; this also include dependencies outside of Python, like 'opusfile-devel' on Fedora. Either way, the debug message pointing out what is missing when compiling PHAzOR (line #16) helps you find out what is needed. Usually those packages have similar enough names to be found quite easily by your package manager of choice.
-# The script below installs the TauonMusicBox repo as a desktop app by cloning files into temporary ones and changing+deploying them; lines #18 to #23. I did it because I was too lazy to learn RPM packaging :B
-# If you're running it for the very first time (and just to be on the safe side), then the repository itself should probably be cleansed, as in:
+# Run this script once you have **ALL OF THE DEPENDENCIES** set up and
+# updated; this also include dependencies **OUTSIDE OF PYTHON**, like
+# 'opusfile-devel' on Fedora.
+# Either way, the debug message pointing out what is missing when
+# compiling PHAzOR (line #16) helps you find out what is needed.
+# Those packages usually have similar enough names to be found quite
+# easily by your package manager of choice.
+#
+# The script below installs the TauonMusicBox repo as a desktop app by
+# cloning files into temporary ones and changing+deploying them;
+# lines #38 to #43. I did it because I was too lazy to learn RPM
+# packaging :B
+#
+# If you're running it for the very first time
+# (and just to be on the safe side), then the repository itself should
+# probably be cleansed, as in:
 #   ----------------------------------
 #   git reset --hard && git clean -dfx
 #   ----------------------------------
+#
+# NEVER run this script as sudo!
 # Thank you.
 
 RepoDir=$(realpath $(dirname "$0")/..)
+ShareDir=~/.local/share
 cd $RepoDir
 
-rm -f lib/*
-sh compile-phazor.sh || exit 0
+set -e
+
+rm -fR lib
+sh compile-phazor.sh
+
+python3 compile-translations.py
 
 cp -f extra/tauonmb.{,tmp.}desktop
 cp -f extra/tauonmb.{,tmp.}sh
@@ -22,9 +42,16 @@ sed -i "s+/opt/tauon-music-box/tauonmb+sh $RepoDir/extra/tauonmb.tmp+g" extra/ta
 sed -i '1a cd $(realpath $(dirname "$0")/..)' extra/tauonmb.tmp.sh
 sed -i "s+/opt/tauon-music-box+$RepoDir+g" extra/tauonmb.tmp.sh
 
-install -Dm755 extra/tauonmb.tmp.desktop ~/.local/share/applications/tauonmb.desktop
-install -Dm644 extra/tauonmb{,-symbolic}.svg ~/.local/share/icons/hicolor/scalable/apps/
+mkdir -p $ShareDir/{applications,icons/hicolor/scalable/apps}
+install -Dm755 extra/tauonmb.tmp.desktop $ShareDir/applications/tauonmb.desktop
+install -Dm644 extra/tauonmb{,-symbolic}.svg $ShareDir/icons/hicolor/scalable/apps/
 
-# This lines uses sudo to update your desktop apps list so you can open it from the menu as soon as it is ready.
-# NEVER run the script itself as sudo, though.
+rm -fR extra/tauonmb.tmp.*
+
+# This lines uses sudo to update your desktop apps list so you can
+# open it from the menu as soon as it is ready.
 sudo update-desktop-database
+
+echo -e "\n== == == == == ==\n\
+ Setup finished!\n\
+== == == == == =="
