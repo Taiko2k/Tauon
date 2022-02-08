dbccf3da307cf04f28eaf17cb0f3ce9e109af82f
Mycus <ledemipa@gmail.com>
2022-02-08 14:39:31 -0400

    Same as `tauonmb.sh` but points to the repo

---
+++ extra/tauonmb-alt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(realpath $(dirname "$0")/..)
+
+if [ "$1" == "--no-start" ]; then
+	if [ "$2" == "--play" ]; then curl http://localhost:7813/play/
+	elif [ "$2" == "--play-pause" ]; then curl http://localhost:7813/playpause/
+	elif [ "$2" == "--pause" ]; then curl http://localhost:7813/pause/
+	elif [ "$2" == "--stop" ]; then curl http://localhost:7813/stop/
+	elif [ "$2" == "--next" ]; then curl http://localhost:7813/next/
+	elif [ "$2" == "--previous" ]; then curl http://localhost:7813/previous/
+	else python3 ../tauon.py "$@";
+	fi
+else
+	python3 ../tauon.py "$@"
+fi
