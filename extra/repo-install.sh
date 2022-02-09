2291d1ed04b9fa74b697d166e0d39c97d68f88c6
Zaragata <ledemipa@gmail.com>
2022-02-09 18:06:31 -0400

    Removes the need for `tauonmb.tmp.sh` (#678)
    * Removed the need for parsing 'extra/tauonmb.sh'
    
    * fix 1 (one) comment

--- extra/repo-install.sh
+++ extra/repo-install.sh
@@ -5,13 +5,13 @@
 # updated; this also include dependencies **OUTSIDE OF PYTHON**, like
 # 'opusfile-devel' on Fedora.
 # Either way, the debug message pointing out what is missing when
-# compiling PHAzOR (line #16) helps you find out what is needed.
+# compiling PHAzOR (line #34) helps you find out what is needed.
 # Those packages usually have similar enough names to be found quite
 # easily by your package manager of choice.
 #
 # The script below installs the TauonMusicBox repo as a desktop app by
 # cloning files into temporary ones and changing+deploying them;
-# lines #38 to #43. I did it because I was too lazy to learn RPM
+# lines #38 to #44. I did it because I was too lazy to learn RPM
 # packaging :B
 #
 # If you're running it for the very first time
@@ -36,17 +36,14 @@ sh compile-phazor.sh
 python3 compile-translations.py
 
 cp -f extra/tauonmb.{,tmp.}desktop
-cp -f extra/tauonmb.{,tmp.}sh
 
-sed -i "s+/opt/tauon-music-box/tauonmb+sh $RepoDir/extra/tauonmb.tmp+g" extra/tauonmb.tmp.desktop
-sed -i '1a cd $(realpath $(dirname "$0")/..)' extra/tauonmb.tmp.sh
-sed -i "s+/opt/tauon-music-box+$RepoDir+g" extra/tauonmb.tmp.sh
+sed -i -e "s+/opt/tauon-music-box/tauonmb.sh %U+python3 $RepoDir/tauon.py+g" -e "s+/opt/tauon-music-box/tauonmb.sh --no-start +curl http://localhost:7813/+g" -e "s+--play-pause+playpause/+g" -e "s+--previous+previous/+g" -e "s+--next+next/+g" -e "s+--stop+stop/+g" extra/tauonmb.tmp.desktop
 
 mkdir -p $ShareDir/{applications,icons/hicolor/scalable/apps}
 install -Dm755 extra/tauonmb.tmp.desktop $ShareDir/applications/tauonmb.desktop
 install -Dm644 extra/tauonmb{,-symbolic}.svg $ShareDir/icons/hicolor/scalable/apps/
 
-rm -fR extra/tauonmb.tmp.*
+rm -fR extra/tauonmb.tmp.desktop
 
 # This lines uses sudo to update your desktop apps list so you can
 # open it from the menu as soon as it is ready.
