d90668f72d9fd88148ee676c12803c0f7042f194
Taiko2k <Taiko2k@users.noreply.github.com>
2022-04-14 16:04:27 +1200

    enable wavpak

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs samplerate libpulse-simple opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 #-Wextra
+gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs samplerate wavpack libpulse-simple opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
 mkdir -p lib
 mv libphazor.so lib/libphazor.so
 
