2b6349eb3da4b5270799494214c663e883c150d8
Pouria Rezaei <Pouria.rz@outlook.com>
2022-05-14 10:30:03 +0430

    Merge PHAZOR compilers

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs samplerate wavpack libpulse-simple opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
-mkdir -p lib
-mv libphazor.so lib/libphazor.so
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs samplerate wavpack libpulse-simple opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
+    mkdir -p lib
+    mv libphazor.so lib/libphazor.so
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs ao wavpack samplerate opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 -D AO -g # -Wextra
+    mkdir -p lib
+    mv libphazor.so lib/libphazor.so
+else
+    echo -e "== == == == == == \n\
+ Unsupported OS. \n\
+== == == == == =="
+    exit 1
+fi
