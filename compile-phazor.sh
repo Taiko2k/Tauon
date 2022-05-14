4310426b471d00ad4211c005037bbfecbb2bd475
Pouria Rezaei <Pouria.rz@outlook.com>
2022-05-14 10:36:45 +0430

    More

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c `pkg-config --cflags --libs samplerate wavpack libpulse-simple opusfile vorbisfile libmpg123 flac libopenmpt` -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
     mkdir -p lib
