4074ba4be20c0be7b168c0da15315e12c007845a
Taiko2k <Taiko2k@users.noreply.github.com>
2022-08-04 02:05:35 +1200

    Fix

--- t_modules/t_main.py
+++ t_modules/t_main.py
@@ -22230,10 +22230,12 @@ def cast_search():
         shooter(cast_search2)
 
 
-x_menu.add_sub(_("Chromecast…"), 220)
+if chrome:
+    x_menu.add_sub(_("Chromecast…"), 220)
+    shooter(cast_search2)
 x_menu.add_to_sub(_("Find Incomplete Albums"), 0, find_incomplete)
 tauon.chrome_menu = x_menu
-shooter(cast_search2)
+
 #x_menu.add(_("Cast…"), cast_search, cast_deco)
 
 
