6eecd368304ae815c08685152697ec6252b51c2b
Taiko2k <Taiko2k@users.noreply.github.com>
2023-04-27 20:37:56 +1200

    fix

--- t_modules/t_main.py
+++ t_modules/t_main.py
@@ -38445,8 +38445,8 @@ class QueueBox:
                         4] and playing_track and playing_track.parent_folder_path == album_parent_path:
                         i = pctl.playlist_playing_position + 1
 
-                   if not item[0] in playlist:
-                       continue
+                    if not item[0] in playlist:
+                        continue
 
                     elif playlist[i] != item[0]:
                         i = playlist.index(item[0])
