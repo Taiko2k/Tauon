0c620a164772b6b45f21de2bcd93d98b0a66b7b3
Taiko2k <Taiko2k@users.noreply.github.com>
2024-04-04 15:10:19 +1300

    disable debug

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -4,7 +4,7 @@ set -e
 
 gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
     `pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt` \
-    -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
+    -shared -o libphazor.so -fPIC -Wall -O3 # -g #-Wextra
 mkdir -p lib
 mv libphazor.so lib/libphazor.so
 
