1d693c37af889bcc59a6dcaef8d0341f2bee107a
Taiko2k <Taiko2k@users.noreply.github.com>
2024-06-21 23:40:17 +1200

    Wip audio playback

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-#gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
-#    `pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme libpipewire-0.3` \
-#    -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
+gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
+    `pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme libpipewire-0.3` \
+    -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
 
 gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
     `pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme libpipewire-0.3` \
     -shared -o libphazor-pipe.so -fPIC -Wall -O3 -g -DPIPE #-Wextra
 
 mkdir -p lib
-#mv libphazor.so lib/libphazor.so
+mv libphazor.so lib/libphazor.so
 mv libphazor-pipe.so lib/libphazor-pipe.so
 
