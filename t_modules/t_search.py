5796cb6394e4e1e15ca1fae274055112354f4415
Martin Rys <martin@rys.rs>
2024-11-09 15:40:46 +0100

    t_search: Add docstrings, fully type the module, fix requests hanging, reduce hadouken, log exceptions

--- t_modules/t_search.py
+++ t_modules/t_search.py
@@ -1,3 +1,4 @@
+"""Tauon Music Box - Search an artist by scrape/fetch module"""
 # Copyright © 2015-2019, Taiko2k captain(dot)gxj(at)gmail.com
 
 #     This file is part of Tauon Music Box.
@@ -14,16 +15,32 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import annotations
 
+import logging
+from typing import TYPE_CHECKING
 
-from bs4 import BeautifulSoup
+if TYPE_CHECKING:
+	from collections.abc import Callable
+
 import requests
+from bs4 import BeautifulSoup
 
-def bandcamp_search(artist, callback=None):
+logging.basicConfig(
+	level=logging.DEBUG,
+	format="%(asctime)s [%(levelname)s] %(message)s",
+	handlers=[
+		logging.StreamHandler(),
+	],
+)
+# INFO and higher to STDERR
+logging.getLogger().handlers[0].setLevel(logging.INFO)
 
+def bandcamp_search(artist: str, callback: Callable[[str | None], None] | None = None) -> str | None:
+	"""Search Bandcamp for the artist and return their URL"""
 	try:
-		page = requests.get("https://bandcamp.com/search?q=" + artist)
-		html = BeautifulSoup(page.text, 'html.parser')
+		page = requests.get(f"https://bandcamp.com/search?q={artist}", timeout=10)
+		html = BeautifulSoup(page.text, "html.parser")
 		results = html.find_all("div", {"class": "result-info"})
 		for result in results:
 			children = result.findChildren("div")
@@ -35,15 +52,14 @@ def bandcamp_search(artist, callback=None):
 			if not okay:
 				continue
 			for child in children:
-				if child["class"][0] == "heading":
-					if child.a.string.strip().lower() == artist.lower():
-						url = child.a["href"].split("?")[0]
-						if callback:
-							callback(url)
-						return url
+				if child["class"][0] == "heading" and child.a.string.strip().lower() == artist.lower():
+					url = child.a["href"].split("?")[0]
+					if callback:
+						callback(url)
+					return url
 
-	except:
-		print("Bandcamp search error")
+	except Exception:
+		logging.exception("Bandcamp search error")
 
 	if callback:
 		callback(None)
