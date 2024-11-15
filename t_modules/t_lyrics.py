ef88869693b022c23ba1d75ddcb1101f51e52185
Martin <martin@rys.rs>
2024-11-15 02:23:29 +0100

    Lyrics module typing, fixes and linting (#1264)
    * t_lyrics: Convert spaces to tabs
    
    * t_lyrics: Type the module, add docstrings, use HTTPStatus, fix hanging requests, reduce hadouken and unify quotes

--- t_modules/t_lyrics.py
+++ t_modules/t_lyrics.py
@@ -1,4 +1,11 @@
-# Tauon Music Box - Lyrics scrape/fetch module
+"""Tauon Music Box - Lyrics scrape/fetch module
+
+You can add lyric providers in this module
+
+Create a function that takes artist and title, and returns lyrics as a str
+If failed to find lyrics, you can return None or raise an exception.
+Finally add provider name and function reference to lyric_sources dict below
+"""
 
 # Copyright © 2018-2023, Taiko2k captain(dot)gxj(at)gmail.com
 
@@ -18,137 +25,128 @@
 #     along with Tauon Music Box.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from unidecode import unidecode
-from bs4 import BeautifulSoup
-import urllib.parse
-import requests
 import re
+import urllib.parse
+from http import HTTPStatus
 
+import requests
+from bs4 import BeautifulSoup
+from unidecode import unidecode
 
-# You can add lyric providers in this module
-
-# Create a function that takes artist and title, and returns lyrics as a str
-# If failed to find lyrics, you can return None or raise an exception.
-# Finally add provider name and function reference to lyric_sources dict below
-
-
-# def lyricwiki(artist, title):
+# def lyricwiki(artist: str, title: str) -> str:
 #
-#     lyrics = PyLyrics.getLyrics(artist, title)
+# 	lyrics = PyLyrics.getLyrics(artist, title)
 #
-#     if lyrics and lyrics[0] == "<" and "Instrumental" in lyrics:
-#         lyrics = "[Instrumental]"
+# 	if lyrics and lyrics[0] == "<" and "Instrumental" in lyrics:
+# 		lyrics = "[Instrumental]"
 #
-#     return lyrics
+# 	return lyrics
 
-def ovh(artist, title):
+def ovh(artist: str, title: str) -> str:
+	"""Get lyrics from lyrics.ovh API"""
+	q = urllib.parse.quote(f"{artist}/{title}")
+	point = f"https://api.lyrics.ovh/v1/{q}"
+	r = requests.get(point, timeout=10)
+	if r.status_code == HTTPStatus.OK:
+		j = r.json()
+		return j["lyrics"]
+	return ""
 
-    q = urllib.parse.quote(f"{artist}/{title}")
-    point = f"https://api.lyrics.ovh/v1/{q}"
-    r = requests.get(point)
-    if r.status_code == 200:
-        j = r.json()
-        return j["lyrics"]
-    return ""
 
-
-# def happi(artist, title):
-#     q = urllib.parse.quote(f"{artist} {title}")
-#     point = f"https://api.happi.dev/v1/music?q={q}&limit=1&apikey=23b23b30Ca5nqZSe5JWJ8I4smmgO1JK6grVTEXpkBz1O8mNjTCmmCjnX&type=track"
-#     r = requests.get(point)
-#     j = r.json()
-#     if not j["result"][0]["haslyrics"]:
-#         return ""
-#     a_id = j["result"][0]["id_artist"]
-#     t_id = j["result"][0]["id_track"]
-#     al_id = j["result"][0]["id_album"]
+# def happi(artist: str, title: str) -> str:
+# 	q = urllib.parse.quote(f"{artist} {title}")
+# 	point = f"https://api.happi.dev/v1/music?q={q}&limit=1&apikey=23b23b30Ca5nqZSe5JWJ8I4smmgO1JK6grVTEXpkBz1O8mNjTCmmCjnX&type=track"
+# 	r = requests.get(point)
+# 	j = r.json()
+# 	if not j["result"][0]["haslyrics"]:
+# 		return ""
+# 	a_id = j["result"][0]["id_artist"]
+# 	t_id = j["result"][0]["id_track"]
+# 	al_id = j["result"][0]["id_album"]
 #
-#     point = f"https://api.happi.dev/v1/music/artists/{a_id}/albums/{al_id}/tracks/{t_id}/lyrics?apikey=23b23b30Ca5nqZSe5JWJ8I4smmgO1JK6grVTEXpkBz1O8mNjTCmmCjnX"
-#     r = requests.get(point)
-#     j = r.json()
-#     return j["result"]["lyrics"]
+# 	point = f"https://api.happi.dev/v1/music/artists/{a_id}/albums/{al_id}/tracks/{t_id}/lyrics?apikey=23b23b30Ca5nqZSe5JWJ8I4smmgO1JK6grVTEXpkBz1O8mNjTCmmCjnX"
+# 	r = requests.get(point)
+# 	j = r.json()
+# 	return j["result"]["lyrics"]
 
-def genius(artist, title, return_url=False):
+def genius(artist: str, title: str, return_url: bool=False) -> str:
+	"""Scrape lyrics from genius.com"""
+	artist = artist.split("feat.")[0]
+	title = title.split("(feat.")[0]
+	line = f"{artist}-{title}"
+	line = re.sub("[,._@!#%^*+:;'()]", "", line)
+	line = line.replace("]", "")
+	line = line.replace("[", "")
+	line = line.replace("?", "")
+	line = line.replace(" ", "-")
+	line = line.replace("/", "-")
+	line = line.replace("-&-", "-and-")
+	line = line.replace("&", "-and-")
+	line = unidecode(line)
+	line = urllib.parse.quote(line)
+	line = f"https://genius.com/{line}-lyrics"
 
-    artist = artist.split('feat.')[0]
-    title = title.split('(feat.')[0]
-    line = f"{artist}-{title}"
-    line = re.sub("[,._@!#%^*+:;'()]", "", line)
-    line = line.replace("]", "")
-    line = line.replace("[", "")
-    line = line.replace("?", "")
-    line = line.replace(" ", "-")
-    line = line.replace("/", "-")
-    line = line.replace("-&-", "-and-")
-    line = line.replace("&", "-and-")
-    line = unidecode(line)
-    line = urllib.parse.quote(line)
-    line = f"https://genius.com/{line}-lyrics"
+	if return_url:
+		return line
 
-    if return_url:
-        return line
+	page = requests.get(line, timeout=10)
+	html = BeautifulSoup(page.text, "html.parser")
 
-    page = requests.get(line)
-    html = BeautifulSoup(page.text, 'html.parser')
+	result = html.find("div", class_="lyrics") #.get_text()
+	if result is not None:
+		lyrics = result.get_text()
+		lyrics2 = []
+		for line in lyrics.splitlines():
+			if line.startswith("["):
+				pass
+			else:
+				lyrics2.append(line)
 
-    result = html.find('div', class_='lyrics') #.get_text()
-    if result is not None:
-        lyrics = result.get_text()
-        lyrics2 = []
-        for line in lyrics.splitlines():
-            if line.startswith("["):
-                pass
-            else:
-                lyrics2.append(line)
+		lyrics = "\n".join(lyrics2)
+		lyrics = lyrics.strip("\n")
+		return lyrics
 
-        lyrics = "\n".join(lyrics2)
-        lyrics = lyrics.strip("\n")
-        return lyrics
+	# New layout type
+	results = html.findAll("div", {"class": lambda l: l and "Lyrics__Container" in l})
+	lyrics = "".join([r.get_text("\n") for r in results])
+	level = 0
+	new = ""
+	for cha in lyrics:
+		if level <= 0:
+			new += cha
+		if cha == "[":
+			level += 1
+		if cha == "]":
+			level -= 1
+	lyrics = new
 
-    # New layout type
-    else:
+	lines = lyrics.splitlines()
+	new_lines = []
+	for line in lines:
+		if "[" in line:
+			line = line.split("[", 1)[0]
+			if line:
+				line += "\n"
 
-        results = html.findAll("div", {"class": lambda l: l and "Lyrics__Container" in l})
-        lyrics = "".join([r.get_text("\n") for r in results])
-        level = 0
-        new = ""
-        for cha in lyrics:
-            if level <= 0:
-                new += cha
-            if cha == "[":
-                level += 1
-            if cha == "]":
-                level -= 1
-        lyrics = new
+		new_lines.append(line.lstrip().rstrip(" ") + "\n")
 
-        lines = lyrics.splitlines()
-        new_lines = []
-        for line in lines:
-            if "[" in line:
-                line = line.split("[", 1)[0]
-                if line:
-                    line += "\n"
-
-            new_lines.append(line.lstrip().rstrip(" ") + "\n")
-
-        lyrics = "".join(new_lines)
-        lyrics = lyrics.replace("(\n", "(")
-        lyrics = lyrics.replace("\n)", ")")
-        lyrics = lyrics.lstrip("\n")
-        lyrics = lyrics.lstrip()
-        return lyrics
-
+	lyrics = "".join(new_lines)
+	lyrics = lyrics.replace("(\n", "(")
+	lyrics = lyrics.replace("\n)", ")")
+	lyrics = lyrics.lstrip("\n")
+	lyrics = lyrics.lstrip()
+	return lyrics
 
 
 lyric_sources = {
-    # "Apiseeds": apiseeds,
-    # "Happi": happi,
-    "Genius": genius,
-    #"LyricWiki": lyricwiki,
-    "lyrics.ovh": ovh,
+	# "Apiseeds": apiseeds,
+	# "Happi": happi,
+	"Genius": genius,
+	#"LyricWiki": lyricwiki,
+	"lyrics.ovh": ovh,
 }
 
 uses_scraping = {
-    #"LyricWiki",
-    "Genius"
+	#"LyricWiki",
+	"Genius",
 }
