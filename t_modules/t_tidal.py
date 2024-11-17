b08474513033e1e5a0d8e7fc5dae808c341d78d6
Martin Rys <martin@rys.rs>
2024-11-17 13:41:35 +0100

    t_tidal: Fully type, use logging module, unify quotes, clean up imports

--- t_modules/t_tidal.py
+++ t_modules/t_tidal.py
@@ -1,39 +1,44 @@
-import io
-from datetime import datetime, timezone
+"""Tauon Music Box - Tidal streaming module"""
+from __future__ import annotations
 
-import requests
+import json
+import logging
+import os
+import webbrowser
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING
+
 allow_tidal = False
 try:
 	import tidalapi
-	from tidalapi import Session
-	from tidalapi import Quality
+	from tidalapi import Quality, Session
 	allow_tidal = True
-except:
-	print("Tidalapi not found")
-import webbrowser
-from pathlib import Path
-import os
-import json
+except Exception:
+	logging.exception("Tidalapi not found")
+
+if TYPE_CHECKING:
+	from t_modules.t_main import Tauon, TrackClass
 
 class Tidal:
 
-	def __init__(self, tauon):
+	def __init__(self, tauon: Tauon) -> None:
 		self.tauon = tauon
 		self.session = None
 		self.save_path = os.path.join(tauon.cache_directory, "tidal.json")
 		self.login_stage = 0
 		self.import_cache = {}
 
-	def logout(self):
+	def logout(self) -> None:
 		self.session = None
 		self.login_stage = 0
 		if os.path.isfile(self.save_path):
 			os.remove(self.save_path)
-	def login1(self):
+
+	def login1(self) -> None:
 		if not allow_tidal:
 			self.tauon.show_message(_("Tidalapi package not loaded"))
 			return
-		print("LOGIN 1")
+		logging.info("LOGIN 1")
 		session = tidalapi.Session()
 		#session.config.pkce_uri_redirect = f"http://localhost:7811/tidalredir"
 		login_url = session.pkce_login_url()
@@ -41,50 +46,50 @@ class Tidal:
 		self.session = session
 		self.login_stage = 2
 
-	def login2(self, url):
-		print("LOGIN 2")
+	def login2(self, url: str) -> None:
+		logging.info("LOGIN 2")
 		if not allow_tidal:
 			return
 		auth_token = self.session.pkce_get_auth_token(url)
 		self.session.process_auth_token(auth_token, is_pkce_token=True)
-		print("login2 done")
+		logging.info("login2 done")
 		self.save_session()
 		self.login_stage = 0
 		# https://tidal.com/android/login/auth?code=eyVyPty.WiGdw-abQ&state=na&appMode=android
 
-	def try_load(self):
+	def try_load(self) -> None:
 		if not allow_tidal:
 			self.tauon.show_message(_("Tidalapi package not loaded"))
 			return
 		if not self.session and os.path.isfile(self.save_path):
-			with open(self.save_path, 'r') as f:
+			with open(self.save_path, "r") as f:
 				session_data = json.load(f)
 			session = tidalapi.Session()
 
 			expiry_time = None
-			if session_data['expiry_time']:
-				expiry_time = datetime.fromisoformat(session_data['expiry_time'])
+			if session_data["expiry_time"]:
+				expiry_time = datetime.fromisoformat(session_data["expiry_time"])
 				# Ensure the datetime is timezone-aware
 				if expiry_time.tzinfo is None:
 					expiry_time = expiry_time.replace(tzinfo=timezone.utc)
 
 			success = session.load_oauth_session(
-				session_data['token_type'],
-				session_data['access_token'],
-				session_data['refresh_token'],
+				session_data["token_type"],
+				session_data["access_token"],
+				session_data["refresh_token"],
 				expiry_time,
-				session_data['is_pkce']
+				session_data["is_pkce"]
 			)
 			if success:
 				self.session = session
 				self.save_session()
-				print("loaded TIDAL login")
+				logging.info("loaded TIDAL login")
 			else:
-				print("Failed to load TIDAL login")
+				logging.error("Failed to load TIDAL login")
 				self.tauon.show_message(_("Failed to load TIDAL login, please try log in again"))
 				os.remove(self.save_path)
 
-	def save_session(self):
+	def save_session(self) -> None:
 		if self.session:
 			session = self.session
 			session_data = {
@@ -94,56 +99,56 @@ class Tidal:
 				"expiry_time": session.expiry_time.isoformat() if session.expiry_time else None,
 				"is_pkce": session.is_pkce
 			}
-			with open(self.save_path, 'w') as f:
+			with open(self.save_path, "w") as f:
 				json.dump(session_data, f)
 
-	def resolve_stream(self, tr):
+	def resolve_stream(self, tr: TrackClass) -> list[str] | None:
 		track_id = tr.url_key
 		self.try_load()
 		if not self.session:
-			print("Tidal: not logged in")
-			return
+			logging.info("Tidal: not logged in")
+			return None
 
 		if self.tauon.prefs.tidal_quality == 2:
-			print("TRY 24 bit flac")
+			logging.info("TRY 24 bit flac")
 			self.session.audio_quality = Quality.hi_res_lossless
 			try:
 				result = self.resolve_stream2(tr)
 				if result:
 					return result
-			except Exception as e:
-				print(str(e))
+			except Exception:
+				logging.exception("Failed to resolve stream")
 
 		if self.tauon.prefs.tidal_quality == 1:
-			print("TRY 16bit flac")
+			logging.info("TRY 16bit flac")
 			self.session.audio_quality = Quality.high_lossless
 			try:
 				result = self.resolve_stream2(tr)
 				if result:
 					return result
-			except Exception as e:
-				print(str(e))
+			except Exception:
+				logging.exception("Failed to resolve stream")
 
-		print("TRY LOW")
+		logging.info("TRY LOW")
 		self.session.audio_quality = Quality.low_320k
 		try:
 			result = self.resolve_stream2(tr)
 			if result:
 				return result
-		except Exception as e:
-			print(str(e))
+		except Exception:
+				logging.exception("Failed to resolve stream")
 
-	def resolve_stream2(self, tr):
+	def resolve_stream2(self, tr: TrackClass) -> list[str] | None:
 		track_id = tr.url_key
 		track = self.session.track(track_id)
-		print("{}: '{}' by '{}'".format(track.id, track.name, track.artist.name))
+		logging.info("{}: '{}' by '{}'".format(track.id, track.name, track.artist.name))
 		stream = track.get_stream()
-		print("MimeType:{}".format(stream.manifest_mime_type))
+		logging.info("MimeType:{}".format(stream.manifest_mime_type))
 
 		manifest = stream.get_stream_manifest()
 		audio_resolution = stream.get_audio_resolution()
 
-		print("track:{}, (quality:{}, codec:{}, {}bit/{}Hz)".format(
+		logging.info("track:{}, (quality:{}, codec:{}, {}bit/{}Hz)".format(
 			track.id,
 			stream.audio_quality,
 			manifest.get_codecs(),
@@ -153,20 +158,20 @@ class Tidal:
 		tr.samplerate = str(audio_resolution[1])
 		tr.bit_depth = audio_resolution[0]
 		if stream.is_MPD:
-			print("MPD!")
+			logging.info("MPD!")
 			return manifest.get_urls()
 		if stream.is_BTS:
-			print("BTS!")
+			logging.info("BTS!")
 			return manifest.get_urls()
 
 		return None
 
-	def build_cache(self):
+	def build_cache(self) -> None:
 		for id, nt in self.tauon.pctl.master_library.items():
 			if nt.url_key and nt.file_ext == "TIDAL":
 				self.import_cache[nt.url_key] = nt
 
-	def new_track(self, track):
+	def new_track(self, track: TrackClass) -> TrackClass:
 
 		new = False
 		nt = self.import_cache.get(track.id)
@@ -201,7 +206,7 @@ class Tidal:
 
 		return nt
 
-	def track(self, id):
+	def track(self, id: int) -> None:
 		self.try_load()
 		if not self.session:
 			return []
@@ -212,7 +217,7 @@ class Tidal:
 		self.tauon.pctl.multi_playlist[self.tauon.pctl.active_playlist_viewing][2].append(nt.index)
 		self.tauon.gui.pl_update += 1
 
-	def fav_albums(self, return_list=False):
+	def fav_albums(self, return_list: bool = False) -> list[TrackClass] | None:
 
 		self.try_load()
 		if not self.session:
@@ -221,9 +226,8 @@ class Tidal:
 
 		try:
 			f = tidalapi.Favorites(self.session, self.session.user.id)
-		except Exception as e:
-			print("Error getting tidal user favorites")
-			print(str(e))
+		except Exception:
+			logging.exception("Error getting tidal user favorites")
 			return []
 
 		playlist = []
@@ -239,7 +243,7 @@ class Tidal:
 		self.tauon.pctl.gen_codes[self.tauon.pl_to_id(len(self.tauon.pctl.multi_playlist) - 1)] = f"tfa"
 		self.tauon.gui.show_message("Playlist load complete", mode="done")
 
-	def fav_tracks(self, return_list=False):
+	def fav_tracks(self, return_list: bool = False) -> list[TrackClass] | None:
 
 		self.try_load()
 		if not self.session:
@@ -248,9 +252,8 @@ class Tidal:
 
 		try:
 			f = tidalapi.Favorites(self.session, self.session.user.id)
-		except Exception as e:
-			print("Error getting tidal user favorites")
-			print(str(e))
+		except Exception:
+			logging.exception("Error getting tidal user favorites")
 			return []
 
 		playlist = []
@@ -265,7 +268,7 @@ class Tidal:
 		self.tauon.pctl.gen_codes[self.tauon.pl_to_id(len(self.tauon.pctl.multi_playlist) - 1)] = f"tft"
 		self.tauon.gui.show_message("Playlist load complete", mode="done")
 
-	def playlist(self, id, return_list=False):
+	def playlist(self, id: int, return_list: bool = False) -> list[TrackClass] | None:
 
 		self.try_load()
 		if not self.session:
@@ -274,9 +277,8 @@ class Tidal:
 
 		try:
 			p = self.session.playlist(id)
-		except Exception as e:
-			print("Error getting tidal playlist")
-			print(str(e))
+		except Exception:
+			logging.exception("Error getting tidal playlist")
 			return []
 
 		playlist = []
@@ -290,7 +292,7 @@ class Tidal:
 		self.tauon.pctl.multi_playlist.append(self.tauon.pl_gen(title=p.name, playlist=playlist))
 		self.tauon.pctl.gen_codes[self.tauon.pl_to_id(len(self.tauon.pctl.multi_playlist) - 1)] = f"tpl\"{id}\""
 
-	def mix(self, id, return_list=False):
+	def mix(self, id: int, return_list: bool = False) -> list[TrackClass] | None:
 
 		self.try_load()
 		if not self.session:
@@ -299,14 +301,13 @@ class Tidal:
 
 		try:
 			p = self.session.mix(id)
-		except Exception as e:
-			print("Error getting tidal mix")
-			print(str(e))
+		except Exception:
+			logging.exception("Error getting tidal mix")
 			return []
 
 		playlist = []
 		for item in p.items():
-			if item.type in ('track', None):  # tracks are None, api bug?
+			if item.type in ("track", None):  # tracks are None, api bug?
 				nt = self.new_track(item)
 				playlist.append(nt.index)
 
@@ -315,7 +316,8 @@ class Tidal:
 
 		self.tauon.pctl.multi_playlist.append(self.tauon.pl_gen(title=p.title, playlist=playlist))
 		self.tauon.pctl.gen_codes[self.tauon.pl_to_id(len(self.tauon.pctl.multi_playlist) - 1)] = f"tmix\"{id}\""
-	def artist(self, id, return_list=False):
+
+	def artist(self, id: int, return_list: bool = False) -> list[TrackClass] | None:
 
 		self.try_load()
 		if not self.session:
@@ -324,9 +326,8 @@ class Tidal:
 
 		try:
 			a = self.session.artist(id)
-		except Exception as e:
-			print("Error getting tidal artist")
-			print(str(e))
+		except Exception:
+			logging.exception("Error getting tidal artist")
 			return []
 
 		playlist = []
@@ -342,7 +343,7 @@ class Tidal:
 		self.tauon.pctl.multi_playlist.append(self.tauon.pl_gen(title=a.name, playlist=playlist))
 		self.tauon.pctl.gen_codes[self.tauon.pl_to_id(len(self.tauon.pctl.multi_playlist) - 1)] = f"tar\"{id}\""
 
-	def append_album(self, id):
+	def append_album(self, id: int) -> None:
 		self.try_load()
 		if not self.session:
 			return
@@ -360,10 +361,10 @@ class Tidal:
 			self.tauon.gui.pl_update += 1
 
 	# def test(self):
-	#	 print("Test Tidal")
+	#	 logging.info("Test Tidal")
 	#	 self.try_load()
 	#	 if not self.session:
-	#		 print("Tidal: not logged in")
+	#		 logging.info("Tidal: not logged in")
 	#		 return
 	#
 	#	 session = self.session
@@ -385,26 +386,26 @@ class Tidal:
 	#	 tracks = album.tracks()
 	#	 # list album tracks
 	#	 for track in tracks:
-	#		 print("{}: '{}' by '{}'".format(track.id, track.name, track.artist.name))
+	#		 logging.info("{}: '{}' by '{}'".format(track.id, track.name, track.artist.name))
 	#		 stream = track.get_stream()
-	#		 print("MimeType:{}".format(stream.manifest_mime_type))
+	#		 logging.info("MimeType:{}".format(stream.manifest_mime_type))
 	#
 	#		 manifest = stream.get_stream_manifest()
 	#		 audio_resolution = stream.get_audio_resolution()
 	#
-	#		 print("track:{}, (quality:{}, codec:{}, {}bit/{}Hz)".format(track.id,
+	#		 logging.info("track:{}, (quality:{}, codec:{}, {}bit/{}Hz)".format(track.id,
 	#																	 stream.audio_quality,
 	#																	 manifest.get_codecs(),
 	#																	 audio_resolution[0],
 	#																	 audio_resolution[1]))
 	#		 if stream.is_MPD:
-	#			 print("MPD!")
+	#			 logging.info("MPD!")
 	#			 # HI_RES_LOSSLESS quality supported when using MPEG-DASH stream (PKCE only!)
 	#			 # 1. Export as MPD manifest
 	#			 mpd = stream.get_manifest_data()
 	#			 # 2. Export as HLS m3u8 playlist
 	#			 hls = manifest.get_hls()
-	#			 print(hls)
+	#			 logging.info(hls)
 	#			 # with open("{}_{}.mpd".format(album_id, track.id), "w") as my_file:
 	#			 #	my_file.write(mpd)
 	#			 # with open("{}_{}.m3u8".format(album_id, track.id), "w") as my_file:
@@ -415,22 +416,22 @@ class Tidal:
 	#				 i = 0
 	#				 for url in urls:
 	#					 i += 1
-	#					 print(i, end=",")
+	#					 logging.info(i, end=",")
 	#					 response = requests.get(url)
 	#					 if response.status_code == 200:
 	#						 f.write(response.content)
 	#					 else:
-	#						 print(f"ERROR CODE: {response.status_code}")
+	#						 logging.error(f"ERROR CODE: {response.status_code}")
 	#				 f.seek(0)
 	#				 with open("test", 'wb') as a:
 	#					 a.write(f.read())
-	#				 print("done")
+	#				 logging.info("done")
 	#
 	#		 if stream.is_BTS:
-	#			 print("BTS!")
+	#			 logging.info("BTS!")
 	#			 # Direct URL (m4a or flac) is available for Quality < HI_RES_LOSSLESS
 	#			 url = manifest.get_urls()
-	#			 print(f"URL = {url}")
+	#			 logging.info(f"URL = {url}")
 	#		 for url in manifest.get_urls():
-	#			 print(url)
+	#			 logging.info(url)
 	#		 break
