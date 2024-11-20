d4d61b7d5a54b592ae072197ee836b96a5615463
Martin Rys <martin@rys.rs>
2024-11-20 17:12:28 +0100

    tauon.py: Upgrade logs, now with colors, on UNIX systems anyway

--- src/tauon/tauon.py
+++ src/tauon/tauon.py
@@ -64,12 +64,41 @@ from sdl2.sdlimage import IMG_Load
 
 from tauon.t_modules import t_bootstrap
 
+
+class CustomLoggingFormatter(logging.Formatter):
+	"""Nicely format logging.loglevel logs"""
+
+	grey        = "\x1b[38;20m"
+	grey_bold   = "\x1b[38;1m"
+	yellow      = "\x1b[33;20m"
+	yellow_bold = "\x1b[33;1m"
+	red         = "\x1b[31;20m"
+	bold_red    = "\x1b[31;1m"
+	reset       = "\x1b[0m"
+	format         = "%(asctime)s [%(levelname)s] [%(module)s] %(message)s"
+	format_verbose = "%(asctime)s [%(levelname)s] [%(module)s] %(message)s (%(filename)s:%(lineno)d)"
+
+	FORMATS = {
+		logging.DEBUG:    grey_bold   + format_verbose + reset,
+		logging.INFO:     yellow      + format         + reset,
+		logging.WARNING:  yellow_bold + format         + reset,
+		logging.ERROR:    red         + format         + reset,
+		logging.CRITICAL: bold_red    + format_verbose + reset,
+	}
+
+	def format(self, record: dict) -> str:
+		log_fmt = self.FORMATS.get(record.levelno)
+		# Remove the miliseconds(%f) from the default string
+		date_fmt = "%Y-%m-%d %H:%M:%S"
+		formatter = logging.Formatter(log_fmt, date_fmt)
+		# Center align + min length things to prevent logs jumping around when switching between different values
+		record.levelname = f"{record.levelname:^7}"
+		record.module = f"{record.module:^10}"
+		return formatter.format(record)
+
 # DEBUG+ to file and std_err
 logging.basicConfig(
 	level=logging.DEBUG,
-#	filename=user_directory + '/crash.log',
-#	format='%(asctime)s %(levelname)s %(name)s %(message)s')
-	format="%(asctime)s [%(levelname)s] [%(module)s] %(message)s",
 	handlers=[
 		logging.StreamHandler(),
 #		logging.FileHandler('/tmp/tauon.log'),
@@ -77,6 +106,7 @@ logging.basicConfig(
 )
 # INFO+ to std_err
 logging.getLogger().handlers[0].setLevel(logging.INFO)
+logging.getLogger().handlers[0].setFormatter(CustomLoggingFormatter())
 
 if sys.platform != "win32":
 	import fcntl
