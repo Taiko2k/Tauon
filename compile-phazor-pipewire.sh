ad1aa0cf66609f3c10e893eed30aa50aa94a2d46
Taiko2k <Taiko2k@users.noreply.github.com>
2024-12-02 20:45:48 +1300

    update phazhor build script

--- compile-phazor-pipewire.sh
+++ compile-phazor-pipewire.sh
@@ -3,8 +3,8 @@
 set -e
 
 gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
-    $(pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme libpipewire-0.3) \
+    $(pkg-config --cflags --libs python3 samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme libpipewire-0.3) \
     -shared -o libphazor-pw.so -fPIC -Wall -O3 -g -DPIPE #-Wextra
 
-mkdir -p lib
-mv libphazor-pw.so lib/libphazor-pw.so
+mkdir -p lib64
+mv libphazor-pw.so lib64/libphazor-pw.so
--- compile-phazor.sh
+++ compile-phazor.sh
@@ -3,8 +3,8 @@
 set -e
 
 gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phazor.c \
-    $(pkg-config --cflags --libs samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme) \
+    $(pkg-config --cflags --libs python3 samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme) \
     -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
 
-mkdir -p lib
-mv libphazor.so lib/libphazor.so
+mkdir -p lib64
+mv libphazor.so lib64/libphazor.so
