4d309e05f405d1a0c930eed875bd5d132cb3fa43
Taiko2k <Taiko2k@users.noreply.github.com>
2024-12-03 13:11:19 +1300

    update manual phazor build scripts

--- compile-phazor.sh
+++ compile-phazor.sh
@@ -6,5 +6,5 @@ gcc src/phazor/kissfft/kiss_fftr.c src/phazor/kissfft/kiss_fft.c src/phazor/phaz
     $(pkg-config --cflags --libs python3 samplerate wavpack opusfile vorbisfile libmpg123 flac libopenmpt libgme) \
     -shared -o libphazor.so -fPIC -Wall -O3 -g #-Wextra
 
-mkdir -p lib64
-mv libphazor.so lib64/libphazor.so
+mkdir -p build
+mv libphazor.so build/libphazor.so
