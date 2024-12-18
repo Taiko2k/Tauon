28e9b185f64243a03a023899a0dee6fd7414e864
Bruno Henrique Paiva <bruno@bruno.codes>
2024-12-18 10:34:44 -0300

    Use Listening activity type in Discord RPC

--- src/tauon/t_modules/t_main.py
+++ src/tauon/t_modules/t_main.py
@@ -34207,6 +34207,7 @@ def discord_loop() -> None:
 					large_image = url
 					small_image = "tauon-standard"
 				RPC.update(
+					activity_type = ActivityType.LISTENING,
 					pid=pid,
 					state=album,
 					details=title,
@@ -34217,6 +34218,7 @@ def discord_loop() -> None:
 			else:
 				#logging.info("Discord RPC - Stop")
 				RPC.update(
+					activity_type = ActivityType.LISTENING,
 					pid=pid,
 					state="Idle",
 					large_image="tauon-standard")
@@ -38760,7 +38762,7 @@ else:
 #	logging.exception("Unable to import rpc, Discord Rich Presence will be disabled.")
 discord_allow = False
 try:
-	from pypresence import Presence
+	from pypresence import Presence, ActivityType
 except ModuleNotFoundError:
 	logging.warning("Unable to import pypresence, Discord Rich Presence will be disabled.")
 except Exception:
