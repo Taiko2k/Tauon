31eff3d3d3ffc9ee9e9991d2567f8264ed53ede6
Martin Rys <martin@rys.rs>
2025-01-27 15:51:02 +0100

    Small Prefs fix up

--- src/tauon/t_modules/t_prefs.py
+++ src/tauon/t_modules/t_prefs.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from pathlib import Path
+
 
 class Prefs:
 	"""Used to hold any kind of settings"""
