3badc196568a44e6d8c0756ea7ec620ff0b19aba
Martin <martin@rys.rs>
2025-02-13 15:42:22 +0100

    Fix broken sdl3 definitions after migration (#1438)

--- src/tauon/t_modules/t_main.py
+++ src/tauon/t_modules/t_main.py
@@ -4920,7 +4920,7 @@ class Tauon:
 		self.worker2_lock                 = threading.Lock()
 		#TODO(Martin) : Fix this by moving the class to root of the module
 		self.cachement:  player4.Cachement | None = None
-		self.dummy_event:          sdl3.SDL_Event = sdl3.SDL_Event()
+		self.dummy_event:     sdl3.sdl3.SDL_Event = sdl3.SDL_Event()
 		self.translate                            = _
 		self.strings                              = Strings()
 		self.gui:                          GuiVar = gui
@@ -26206,7 +26206,7 @@ def clear_img_cache(delete_disk: bool = True) -> None:
 			break
 
 	for key, value in tauon.gall_ren.gall.items():
-		SDL_DestroyTexture(value[2])
+		sdl3.SDL_DestroyTexture(value[2])
 	tauon.gall_ren.gall = {}
 
 	if delete_disk:
@@ -26220,7 +26220,7 @@ def clear_img_cache(delete_disk: bool = True) -> None:
 	prefs.failed_artists.clear()
 	for key, value in artist_list_box.thumb_cache.items():
 		if value:
-			SDL_DestroyTexture(value[0])
+			sdl3.SDL_DestroyTexture(value[0])
 	artist_list_box.thumb_cache.clear()
 	gui.update += 1
 
@@ -26244,7 +26244,7 @@ def clear_track_image_cache(track: TrackClass):
 	keys = set()
 	for key, value in tauon.gall_ren.gall.items():
 		if key[0] == track:
-			SDL_DestroyTexture(value[2])
+			sdl3.SDL_DestroyTexture(value[2])
 			if key not in keys:
 				keys.add(key)
 	for key in keys:
@@ -36423,7 +36423,7 @@ def toggle_titlebar_line(mode: int = 0) -> bool | None:
 		return update_title
 
 	line = window_title
-	SDL_SetWindowTitle(t_window, line)
+	sdl3.SDL_SetWindowTitle(t_window, line)
 	update_title ^= True
 	if update_title:
 		update_title_do()
