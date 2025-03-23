28ab841989322372724f60315e90a7eaf99404ba
Taiko2k <Taiko2k@users.noreply.github.com>
2025-03-24 12:29:11 +1300

    revert sdl3 dl for all platforms

--- src/tauon/__main__.py
+++ src/tauon/__main__.py
@@ -207,11 +207,11 @@ if Path(user_directory / "x11").exists():
 	os.environ["SDL_VIDEODRIVER"] = "x11"
 
 # We currently only properly package SDL3 on Windows and macOS, remove the if check when Linux is fixed
-#if sys.platform in ("win32", "darwin"):
-os.environ["SDL_BINARY_PATH"]              = "." # Set the path to your binaries,               "sdl3/bin" by default.
-os.environ["SDL_DISABLE_METADATA"]         = "1" # Disable metadata method,                     "0"        by default.
-os.environ["SDL_CHECK_BINARY_VERSION"]     = "0" # Disable binary version checking,             "1"        by default.
-os.environ["SDL_IGNORE_MISSING_FUNCTIONS"] = "1" # Disable missing function warnings,           "0"        by default.
+if sys.platform in ("win32", "darwin"):
+	os.environ["SDL_BINARY_PATH"]              = "." # Set the path to your binaries,               "sdl3/bin" by default.
+	os.environ["SDL_DISABLE_METADATA"]         = "1" # Disable metadata method,                     "0"        by default.
+	os.environ["SDL_CHECK_BINARY_VERSION"]     = "0" # Disable binary version checking,             "1"        by default.
+	os.environ["SDL_IGNORE_MISSING_FUNCTIONS"] = "1" # Disable missing function warnings,           "0"        by default.
 if pyinstaller_mode:
 	os.environ["SDL_FIND_BINARIES"]            = "0" # Search for binaries in the system libraries, "1"        by default.
 
