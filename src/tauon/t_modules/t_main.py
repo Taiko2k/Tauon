24aa1604dddb57827e4dfe510895a2ce7746afab
rexendevar <46948713+rexendevar@users.noreply.github.com>
2025-06-12 11:31:37 -0700

    Update t_main.py
    i think i have to commit something for it to build anything

--- src/tauon/t_modules/t_main.py
+++ src/tauon/t_modules/t_main.py
@@ -19,7 +19,7 @@ I would highly recommend not using this project as an example on how to code cle
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
