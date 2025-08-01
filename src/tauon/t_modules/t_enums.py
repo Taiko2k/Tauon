b8bd5e183f311dd84daea73955e288e90238be79
Martin Rys <martin@rys.rs>
2025-08-02 01:25:19 +0200

    Enumize player4_state

--- src/tauon/t_modules/t_enums.py
+++ src/tauon/t_modules/t_enums.py
@@ -2,7 +2,14 @@ from enum import IntEnum
 
 
 class LoaderCommand(IntEnum):
-	NONE = 0
-	DONE = 1
+	NONE   = 0
+	DONE   = 1
 	FOLDER = 2
-	FILE = 3
+	FILE   = 3
+
+class PlayerState(IntEnum):
+	STOPPED      = 0
+	PLAYING      = 1
+	PAUSED       = 2
+	URL_STREAM   = 3
+	SPOTIFY_MODE = 4
