715ed40df2e8c8df273af11eaf113585d2982dc1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-19 16:36:38 -0400

    improve launcher script (#2077)

--- extra/tauonmb.sh
+++ extra/tauonmb.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-if [ "$1" == "--no-start" ]; then
-	if [ "$2" == "--play" ];         then curl http://localhost:7813/play/
-	elif [ "$2" == "--play-pause" ]; then curl http://localhost:7813/playpause/
-	elif [ "$2" == "--pause" ];      then curl http://localhost:7813/pause/
-	elif [ "$2" == "--stop" ];       then curl http://localhost:7813/stop/
-	elif [ "$2" == "--next" ];       then curl http://localhost:7813/next/
-	elif [ "$2" == "--previous" ];   then curl http://localhost:7813/previous/
-	else tauonmb "$@";
-	fi
-else
-	tauonmb "$@"
+#!/bin/sh
+if [ "$1" = '--no-start' ]; then
+	case "$2" in
+		--play)       exec curl http://localhost:7813/play/;;
+		--play-pause) exec curl http://localhost:7813/playpause/;;
+		--pause)      exec curl http://localhost:7813/pause/;;
+		--stopcurl)   exec curl http://localhost:7813/stop/;;
+		--next)       exec curl http://localhost:7813/next/;;
+		--previous)   exec curl http://localhost:7813/previous/;;
+	esac
 fi
+exec tauonmb "$@"
