ca90e4b84bc6bb218d8b053b5b8a0927ae92ecb8
Yisus Pineapple <jesuspina0930@gmail.com>
2026-04-30 19:12:52 -0400

    fix: reemplazar openmpt_stream_get_file_callbacks2 por version compatible con Debian 12

--- src/phazor/phazor.c
+++ src/phazor/phazor.c
@@ -2444,7 +2444,7 @@ int load_next() {
 
 	if (codec == MPT) {
 		mod_file = uni_fopen(loaded_target_file);
-		mod = openmpt_module_create2(openmpt_stream_get_file_callbacks2(), mod_file, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+		mod = openmpt_module_create2(openmpt_stream_get_file_callbacks(), mod_file, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 		src_channels = 2;
 		fclose(mod_file);
 
