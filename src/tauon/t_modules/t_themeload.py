5ab329998c98124c251b6648ad00714725c8d361
Flynn <46948713+rexendevar@users.noreply.github.com>
2026-06-18 14:30:40 -0700

    More themeable colors, reorganize theme editor (#2193)
    * more themeable colors and reorganize theme editor
    
    * unpotatoify the code
    
    * clear up duplicate entries
    
    * remove dupe again

--- src/tauon/t_modules/t_themeload.py
+++ src/tauon/t_modules/t_themeload.py
@@ -130,6 +130,17 @@ THEME_SERIALISABLE_COLOURS: tuple[tuple[str, str], ...] = (
 	("column bar background", "column_bar_background"),
 	("artist bio background", "artist_bio_background"),
 	("artist bio text", "artist_bio_text"),
+	("visualizer bg", "vis_bg"),
+	("link text", "link_text"),
+	("star line playing", "star_line_playing"),
+	("message box bg", "message_box_bg"),
+	("message box text", "message_box_text"),
+	("queue drag indicator", "queue_drag_indicator_colour"),
+	("pulse colour", "pulse_colour"),
+	("queue card background", "queue_card_background"),
+	("column grip", "column_grip"),
+	("column bar text", "column_bar_text"),
+	("box thumb background", "box_thumb_background"),
 )
 
 
@@ -234,25 +245,23 @@ def load_theme(colours: ColoursClass, path: Path) -> None:
 				continue
 			if p.startswith("deco="):
 				colours.deco = p.split("=", 1)[1].strip()
-			if "light-mode" in p:
+			elif "light-mode" in p:
 				colours.light_mode()
-			if "window frame" in p:
+			elif "window frame" in p:
 				colours.window_frame = get_colour_from_line(p)
-			if "gallery highlight" in p:
+			elif "gallery highlight" in p:
 				colours.gallery_highlight = get_colour_from_line(p)
-			if "index playing" in p:
+			elif "index playing" in p:
 				colours.index_playing = get_colour_from_line(p)
-			if "time playing" in p:
+			elif "time playing" in p:
 				colours.time_text = get_colour_from_line(p)
-			if "artist playing" in p:
+			elif "artist playing" in p:
 				colours.artist_playing = get_colour_from_line(p)
-			if "album line" in p:  # Bad name
+			elif "album line" in p or "track album" in p:  # Bad name
 				colours.album_text = get_colour_from_line(p)
-			if "track album" in p:
-				colours.album_text = get_colour_from_line(p)
-			if "album playing" in p:
+			elif "album playing" in p:
 				colours.album_playing = get_colour_from_line(p)
-			if "top panel" in p or "player background" in p:
+			elif "top panel" in p or "player background" in p:
 				colours.top_panel_background = get_colour_from_line(p)
 
 				if test_lumi(colours.bottom_panel_colour) < 0.2:
@@ -262,207 +271,217 @@ def load_theme(colours: ColoursClass, path: Path) -> None:
 				else:
 					colours.corner_icon = ColourRGBA(255, 255, 255, 30)
 
-				if test_lumi(colours.bottom_panel_colour) < 0.2:
-					colours.corner_icon = ColourRGBA(0, 0, 0, 60)
-
 				if not colours.lm:
 					colours.corner_button = rgb_add_hls(colours.top_panel_background, 0, 0.18, 0)
-			if "status text" in p:
+			elif "status text" in p:
 				status_text_temp = get_colour_from_line(p)
 				colours.status_text_over = status_text_temp
 				colours.status_text_normal = rgb_add_hls(status_text_temp, 0, 0.30, -0.15)
 
-			if "corner button off" in p:
+			elif "corner button off" in p:
 				colours.corner_button = get_colour_from_line(p)
-			if "corner button on" in p:
+			elif "corner button on" in p:
 				colours.corner_button_active = get_colour_from_line(p)
-			if "menu button normal" in p:
+			elif "menu button normal" in p:
 				colours.status_text_normal = get_colour_from_line(p)
 				status_text_color_defined = 1
-			if "menu button hover" in p:
+			elif "menu button hover" in p:
 				colours.status_text_over = get_colour_from_line(p)
 				status_text_color_defined = 1
-			if "queue panel" in p:
+			elif "queue panel" in p:
 				colours.queue_background = get_colour_from_line(p)
-			if "side panel" in p:
+			elif "side panel" in p:
 				colours.side_panel_background = get_colour_from_line(p)
 				colours.playlist_box_background = colours.side_panel_background
-			if "lyrics panel" in p:
+			elif "lyrics panel" in p:
 				colours.lyrics_panel_background = get_colour_from_line(p)
 				lyrics_panel_color_defined = 1
-			if "gallery background" in p:
+			elif "gallery background" in p:
 				colours.gallery_background = get_colour_from_line(p)
-			if "playlist panel" in p:  # bad name
+			elif "playlist panel" in p or "tracklist panel" in p:  # bad name
 				colours.playlist_panel_background = get_colour_from_line(p)
-			if "tracklist panel" in p:
-				colours.playlist_panel_background = get_colour_from_line(p)
-			if "track line" in p:
+			elif "track line" in p:
 				colours.title_text = get_colour_from_line(p)
-			if "track missing" in p:
+			elif "track missing" in p:
 				colours.playlist_text_missing = get_colour_from_line(p)
-			if "playing highlight" in p:
+			elif "playing highlight" in p:
 				colours.row_playing_highlight = get_colour_from_line(p)
-			if "track time" in p:
+			elif "track time" in p:
 				colours.bar_time = get_colour_from_line(p)
-			if "fav line" in p:
+			elif "fav line" in p:
 				colours.star_line = get_colour_from_line(p)
-			if "folder title" in p:
+			elif "folder title" in p:
 				colours.folder_title = get_colour_from_line(p)
-			if "folder line" in p:
+			elif "folder line" in p:
 				colours.folder_line = get_colour_from_line(p)
-			if "buttons off" in p:
+			elif "buttons off" in p:
 				colours.media_buttons_off = get_colour_from_line(p)
-			if "buttons over" in p:
+			elif "buttons over" in p:
 				colours.media_buttons_over = get_colour_from_line(p)
-			if "buttons active" in p:
+			elif "buttons active" in p:
 				colours.media_buttons_active = get_colour_from_line(p)
-			if "playing time" in p:
+			elif "playing time" in p:
 				colours.time_playing = get_colour_from_line(p)
-			if "track index" in p:
+			elif "track index" in p:
 				colours.index_text = get_colour_from_line(p)
-			if "track playing" in p:
+			elif "track playing" in p:
 				colours.title_playing = get_colour_from_line(p)
-			if "select highlight" in p:
+			elif "select highlight" in p:
 				colours.row_select_highlight = get_colour_from_line(p)
-			if "track artist" in p:
+			elif "track artist" in p:
 				colours.artist_text = get_colour_from_line(p)
-			if "tab active line" in p:  # bad name
+			elif "tab active line" in p or "tab active text" in p:  # bad name
 				colours.tab_text_active = get_colour_from_line(p)
-			if "tab line" in p:  # bad name
+			elif "tab line" in p or "tab text" in p:  # bad name
 				colours.tab_text = get_colour_from_line(p)
-			if "tab active text" in p:
-				colours.tab_text_active = get_colour_from_line(p)
-			if "tab text" in p:
-				colours.tab_text = get_colour_from_line(p)
-			if "tab background" in p:
+			elif "tab background" in p:
 				colours.tab_background = get_colour_from_line(p)
-			if "tab over" in p:
+			elif "tab over" in p:
 				colours.tab_highlight = get_colour_from_line(p)
-			if "tab active background" in p:
+			elif "tab active background" in p:
 				colours.tab_background_active = get_colour_from_line(p)
-			if "title info" in p:
+			elif "title info" in p:
 				colours.side_bar_line1 = get_colour_from_line(p)
-			if "extra info" in p:
+			elif "extra info" in p:
 				colours.side_bar_line2 = get_colour_from_line(p)
-			if "bottom title" in p:
+			elif "bottom title" in p:
 				colours.bar_title_text = get_colour_from_line(p)
-			if "scroll bar" in p:
+			elif "scroll bar" in p:
 				colours.scroll_colour = get_colour_from_line(p)
-			if "seek bar" in p:
+			elif "seek bar" in p:
 				colours.seek_bar_fill = get_colour_from_line(p)
-			if "seek bg" in p:
+			elif "seek bg" in p:
 				colours.seek_bar_background = get_colour_from_line(p)
-			if "volume bar" in p:
+			elif "volume bar" in p:
 				colours.volume_bar_fill = get_colour_from_line(p)
-			if "volume bg" in p:
+			elif "volume bg" in p:
 				colours.volume_bar_background = get_colour_from_line(p)
-			if "mode off" in p:
+			elif "mode off" in p:
 				colours.mode_button_off = get_colour_from_line(p)
-			if "mode over" in p:
+			elif "mode over" in p:
 				colours.mode_button_over = get_colour_from_line(p)
-			if "mode on" in p:
+			elif "mode on" in p:
 				colours.mode_button_active = get_colour_from_line(p)
-			if "art border" in p:
+			elif "art border" in p:
 				colours.art_box = get_colour_from_line(p)
-			if "tb line" in p:
+			elif "tb line" in p:
 				colours.tb_line = get_colour_from_line(p)
-			if "music vis" in p:
+			elif "music vis" in p:
 				colours.vis_colour = get_colour_from_line(p)
-			if "menu background" in p:
+			elif "menu background" in p or "menu bg" in p:
 				colours.menu_background = get_colour_from_line(p)
-			if "menu text" in p:
+			elif "menu text" in p:
 				colours.menu_text = get_colour_from_line(p)
-			if "menu disable" in p:
+			elif "menu disable" in p:
 				colours.menu_text_disabled = get_colour_from_line(p)
-			if "menu icons" in p:
+			elif "menu icons" in p:
 				colours.menu_icons = get_colour_from_line(p)
-			if "menu highlight" in p:
+			elif "menu highlight" in p:
 				colours.menu_highlight_background = get_colour_from_line(p)
-			if "menu border" in p:
+			elif "menu border" in p:
 				colours.menu_tab = get_colour_from_line(p)
-			if "lyrics showcase" in p or "lyrics text" in p:
+			elif "lyrics showcase" in p or "lyrics text" in p:
 				colours.lyrics = get_colour_from_line(p)
-			if "active lyric" in p:
+			elif "active lyric" in p:
 				colours.active_lyric = get_colour_from_line(p)
-			if "bottom panel" in p:
+			elif "bottom panel" in p:
 				colours.bottom_panel_colour = get_colour_from_line(p)
 				# colours.menu_background = colours.bottom_panel_colour
-			if "mini bg" in p:
+			elif "mini bg" in p:
 				colours.mini_mode_background = get_colour_from_line(p)
-			if "mini border" in p:
+			elif "mini border" in p:
 				colours.mini_mode_border = get_colour_from_line(p)
-			if "mini text 1" in p:
+			elif "mini text 1" in p:
 				colours.mini_mode_text_1 = get_colour_from_line(p)
-			if "mini text 2" in p:
+			elif "mini text 2" in p:
 				colours.mini_mode_text_2 = get_colour_from_line(p)
-			if "column-" in p:
+			elif "column-" in p:
 				key = p[p.find("column-") + 7 :].replace("-", " ").lower().title().rstrip()
 				value = get_colour_from_line(p)
 				colours.column_colours[key] = value
-			if "column+" in p:
+			elif "column+" in p:
 				key = p[p.find("column+") + 7 :].replace("-", " ").lower().title().rstrip()
 				value = get_colour_from_line(p)
 				colours.column_colours_playing[key] = value
-			if "menu bg" in p:
-				colours.menu_background = get_colour_from_line(p)
-			if "playlist box bg" in p:  # bad name
-				colours.playlist_box_background = get_colour_from_line(p)
-			if "playlist background" in p:
+			elif "playlist box bg" in p or "playlist background" in p:  # bad name
 				colours.playlist_box_background = get_colour_from_line(p)
 
-			if "box background" in p:
+			elif "box background" in p:
 				colours.box_background = get_colour_from_line(p)
-			if "box border" in p:
+			elif "box border" in p:
 				colours.box_border = get_colour_from_line(p)
-			if "box text border" in p:
+			elif "box text border" in p:
 				colours.box_text_border = get_colour_from_line(p)
-			if "box text label" in p:
+			elif "box text label" in p:
 				colours.box_text_label = get_colour_from_line(p)
 
-			if "box title text" in p:
+			elif "box title text" in p:
 				colours.box_title_text = get_colour_from_line(p)
-			if "box text normal" in p:
+			elif "box text normal" in p:
 				colours.box_text = get_colour_from_line(p)
-			if "box sub text" in p:
+			elif "box sub text" in p:
 				colours.box_sub_text = get_colour_from_line(p)
-			if "box input text" in p:
+			elif "box input text" in p:
 				colours.box_input_text = get_colour_from_line(p)
 
-			if "box button text highlight" in p:
+			elif "box button text highlight" in p:
 				colours.box_button_text_highlight = get_colour_from_line(p)
-			if "box button text normal" in p:
+			elif "box button text normal" in p:
 				colours.box_button_text = get_colour_from_line(p)
-			if "box button background normal" in p:
+			elif "box button background normal" in p:
 				colours.box_button_background = get_colour_from_line(p)
-			if "box button background highlight" in p:
+			elif "box button background highlight" in p:
 				colours.box_button_background_highlight = get_colour_from_line(p)
-			if "box button border" in p:
+			elif "box button border" in p:
 				colours.box_check_border = get_colour_from_line(p)
 
-			if "window buttons background" in p:
+			elif "window buttons background" in p:
 				colours.window_buttons_bg = get_colour_from_line(p)
-			if "window buttons on" in p:
+			elif "window buttons on" in p:
 				colours.window_buttons_bg_over = get_colour_from_line(p)
-			if "window buttons icon off" in p:
+			elif "window buttons icon off" in p:
 				colours.window_button_icon_off = get_colour_from_line(p)
 				colours.window_button_x_off = colours.window_button_icon_off
-			if "window buttons icon over" in p:
+			elif "window buttons icon over" in p:
 				colours.window_buttons_icon_over = get_colour_from_line(p)
 				colours.window_button_x_on = colours.window_buttons_icon_over
-			if "window button x on" in p:
+			elif "window button x on" in p:
 				colours.window_button_x_on = get_colour_from_line(p)
-			if "window button x off" in p:
+			elif "window button x off" in p:
 				colours.window_button_x_off = get_colour_from_line(p)
-			if "column bar background" in p:
+			elif "column bar background" in p:
 				colours.column_bar_background = get_colour_from_line(p)
 
-			if "artist bio background" in p:
+			elif "artist bio background" in p:
 				colours.artist_bio_background = get_colour_from_line(p)
-			if "artist bio text" in p:
+			elif "artist bio text" in p:
 				colours.artist_bio_text = get_colour_from_line(p)
-			# if "panel button off" in p:
+
+			elif "visualizer bg" in p:
+				colours.vis_bg = get_colour_from_line(p)
+			elif "link text" in p:
+				colours.link_text = get_colour_from_line(p)
+			elif "star line playing" in p:
+				colours.star_line_playing = get_colour_from_line(p)
+			elif "message box bg" in p:
+				colours.message_box_bg = get_colour_from_line(p)
+			elif "message box text" in p:
+				colours.message_box_text = get_colour_from_line(p)
+			elif "queue drag indicator" in p:
+				colours.queue_drag_indicator_colour = get_colour_from_line(p)
+			elif "pulse colour" in p:
+				colours.pulse_colour = get_colour_from_line(p)
+			elif "queue card background" in p:
+				colours.queue_card_background = get_colour_from_line(p)
+			elif "column grip" in p:
+				colours.column_grip = get_colour_from_line(p)
+			elif "column bar text" in p:
+				colours.column_bar_text = get_colour_from_line(p)
+			elif "box thumb background" in p:
+				colours.box_thumb_background = get_colour_from_line(p)
+			# elif "panel button off" in p:
 			# 	colours.corner_button = get_colour_from_line(p)
-			# if "panel button on" in p:
+			# elif "panel button on" in p:
 			# 	colours.corner_button_active = get_colour_from_line(p)
 	colours.post_config()
 	if colours.lm:
